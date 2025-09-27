9cf6887fa0ed9c63b87c0ac08ea929d4464012cd
Zachary James <140269823+Yuplox@users.noreply.github.com>
2025-09-27 00:45:09 -0400

    Implemented mergeSort and insertionSort

--- sorter.hpp
+++ sorter.hpp
@@ -1,13 +1,11 @@
-#ifndef SORTER_H
-#define SORTER_H
+#ifndef SORTER_HPP
+#define SORTER_HPP
 
 #include <vector>
 
 class Sorter {
 public:
-    // Comparison Sorting Algorithms
-
-    template<typename T>
+    template <typename T>
     static std::vector<T>& selectionSort(std::vector<T>& nums) {
         size_t length = nums.size();
         for (size_t i = 0; i < length; ++i) {
@@ -15,15 +13,15 @@ public:
             for (size_t j = i; j < length; ++j) {
                 if (nums[j] < nums[minInd])
                     minInd = j;
-                }
-                T temp = nums[i];
-                nums[i] = nums[minInd];
-                nums[minInd] = temp;
             }
+            T temp = nums[i];
+            nums[i] = nums[minInd];
+            nums[minInd] = temp;
+        }
         return nums;
     }
 
-    template<typename T>
+    template <typename T>
     static std::vector<T>& bubbleSort(std::vector<T>& nums) {
         size_t length = nums.size();
         for (size_t i = length - 1; i > 0; --i) {
@@ -38,29 +36,88 @@ public:
         return nums;
     }
 
-    template<typename T>
-    static std::vector<T>& insertionSort(std::vector<T>& nums);
+    template <typename T>
+    static std::vector<T>& insertionSort(std::vector<T>& nums) {
+        size_t length = nums.size();
+        for (size_t i = 1; i < length; ++i) {
+            T key = nums[i];
 
-    template<typename T>
+            // Inserts elements from index 1 to i - 1
+            size_t j = i - 1;
+            while(j > 0 && key < nums[j]) {
+                nums[j + 1] = nums[j];
+                --j;
+            }
+
+            // Because j is type size_t and can underflow
+            // Index 0 is handled outside the loop
+            if (j == 0 && key < nums[0]) {
+                nums[1] = nums[0];
+                nums[0] = key;
+            } else {
+                nums[j + 1] = key; 
+            }
+        }
+        return nums;
+    }
+
+    template <typename T>
     static std::vector<T>& quickSort(std::vector<T>& nums);
 
-    template<typename T>
-    static std::vector<T>& mergeSort(std::vector<T>& nums);
+    template <typename T>
+    static std::vector<T>& mergeSort(std::vector<T>& nums) {
+        return mergeSortHelper(nums, 0, nums.size());
+    }
 
-    template<typename T>
+    template <typename T>
     static std::vector<T>& heapSort(std::vector<T>& nums);
 
-    // Non-Comparison Sorting Algorithms
-    template<typename T>
+    template <typename T>
     static std::vector<T>& radixSort(std::vector<T>& nums);
 
 private:
-    template<typename T>
+    template <typename T>
     static std::vector<T>& countingSort(std::vector<T>& nums);
     
-    template<typename T>
+    template <typename T>
     static std::vector<T>& quickSortHelper(std::vector<T>& nums);
-};
 
+    template <typename T>
+    static std::vector<T>& mergeSortHelper(std::vector<T>& nums, size_t begin, size_t end) {
+        if (end - begin <= 1)
+            return nums;
+        
+        size_t mid = begin + (end - begin) / 2;
+        mergeSortHelper(nums, begin, mid);
+        mergeSortHelper(nums, mid, end);
+
+        std::vector<T> left(nums.begin() + begin, nums.begin() + mid);
+        std::vector<T> right(nums.begin() + mid, nums.begin() + end);
+        size_t li = 0;
+        size_t ri = 0;
+
+        while (li < left.size() && ri < right.size()) {
+            if (left[li] <= right[ri]) {
+                nums[begin + li + ri] = left[li];
+                ++li;
+            } else {
+                nums[begin + li + ri] = right[ri];
+                ++ri;
+            }
+        }
+
+        while (li < left.size()) {
+            nums[begin + li + ri] = left[li];
+            ++li;
+        }
+
+        while (ri < right.size()) {
+            nums[begin + li + ri] = right[ri];
+            ++ri;
+        }
+
+        return nums;
+    }
+};
 
 #endif
--- test.cpp
+++ test.cpp
@@ -2,19 +2,26 @@
 #include "sorter.hpp"
 
 int main() {
-    std::vector<int> nums1 = {55, 88, 23, 10, 90, 33, 79, 1, 30, 55, 100};
-    std::vector<int> nums2 = {55, 88, 23, 10, 90, 33, 79, 1, 30, 55, 100};
+    std::vector nums1 = {55, 88, 23, 10, 90, 33, 79, 1, 30, 55, 100};
+    std::vector nums2 = {55.1, 88.0, 23.0, 10.0, 90.0, 33.0, 79.0, 1.0, 30.0, 55.0, 100.0};
+    std::vector nums3 = {55, 88, 23, 10, 90, 33, 79, 1, 30, 55, 100};
+
+    Sorter::selectionSort(nums1);
+    Sorter::bubbleSort(nums2);
+    Sorter::mergeSort(nums3);
 
 
-    Sorter::selectionSort<int>(nums1);
-    Sorter::bubbleSort<int>(nums2);
-
-    for (int n : nums1) {
+    for (auto n : nums1) {
         std::cout << n << " "; 
     }
     std::cout << std::endl;
 
-    for (int n : nums2) {
+    for (auto n : nums2) {
+        std::cout << n << " "; 
+    }
+    std::cout << std::endl;
+
+    for (auto n : nums3) {
         std::cout << n << " "; 
     }
     std::cout << std::endl;
